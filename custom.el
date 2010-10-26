c358b0f744bab4a88dd87c2c6e98faaccca0ac1e
Sam Merritt <spam@andcheese.org>
2010-10-25 18:04:29 -0700

    A few more basic customizations

--- custom.el
+++ custom.el
@@ -4,6 +4,22 @@
 (random t)                              ; reseed
 (server-start)
 (global-auto-revert-mode 1)
+(set-cursor-color "black")
+
+;; Mac-isms. They do no harm on non-macs.
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+
+;; don't iconify on C-z when running in X
+;; or exit emacs (!) when running in Emacs.app
+(when window-system (global-unset-key "\C-z"))
+
+;; while I <square box> Unicode as much as the next guy,
+;; I want my lambdas left alone.
+(remove-hook 'coding-hook 'pretty-lambdas)
+
+;; just nice to have everywhere; my screen is only so wide
+(add-hook 'coding-hook (lambda () (setq tab-width 2)))
 
 ;; bar cursor makes it easier to see what delete-(backward-)char are going to hit
 (add-to-list 'load-path "~/.emacs.d/vendor/bar-cursor")
