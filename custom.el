4f8fb8b2ebd7a48883ee1241763e1a798215f0de
Sam Merritt <spam@andcheese.org>
2010-10-25 17:55:57 -0700

    Basic customizations

---
+++ custom.el
@@ -0,0 +1,7 @@
+;;;; Basic setup: colors, modifier keys, builtin behaviors, etc.
+(setq kill-whole-line t)
+(setq confirm-kill-emacs 'yes-or-no-p)
+(random t)                              ; reseed
+(server-start)
+(global-auto-revert-mode 1)
+
